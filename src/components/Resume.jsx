9dd30605aa537a66e7e580ae8589ab900f8791e9
Tyler Clark <tylerc111@gmail.com>
2024-04-15 16:26:27 -0400

    expanded on Resume and added headshot to file

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,13 +4,20 @@ const Resume = () => {
   return (
     <section id="resume">
       <h2>Resume</h2>
-      <p>Download my resume <a href="path_to_resume" download>here</a>.</p>
+      <p>Download my resume <a href="https://docs.google.com/document/d/1CEkUX6B6OrPmkJeaGCj1shRERKnSm5WaOFM7pVKWtM8/edit" download>here</a>.</p>
       <h3>Proficiencies</h3>
       <ul>
-        <li>Proficiency 1</li>
-        <li>Proficiency 2</li>
-        <li>Proficiency 3</li>
-        {/* Add more proficiencies here */}
+        <li>HTML, CSS, and JavaScript</li>
+        <li>React</li>
+        <li>Git & GitHub</li>
+        <li>Express JS</li>
+        <li>Node JS</li>
+        <li>SQL</li>
+        <li>MVC</li>
+        <li>MongoDB</li>
+        <li>NoSQL</li>
+        <li>PWA</li>
+        <li>MERN</li>
       </ul>
     </section>
   );
