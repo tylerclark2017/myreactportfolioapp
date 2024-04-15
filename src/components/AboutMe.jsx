060b4ac08576d649024c8972e653012ce15608e3
Tyler Clark <tylerc111@gmail.com>
2024-04-15 16:41:23 -0400

    added dependency for photo implementation and added it to vite config file

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Headshot from './images/ProfessionalHeadshot.jpg';
 
 const AboutMe = () => {
   return (
     <section id="about-me">
       <h2>About Me</h2>
       <div className="bio">
-        <img src="path_to_photo" alt="Developer's Photo" />
+        <img src= { Headshot } alt="Tyler's Professional Headshot" />
         <p>I am a recent graduate of edX and George Washington University's Full Stack Coding Bootcamp.
             In this bootcamp, we learned numerous languages and concepts that are the building blocks of front and back end 
-            web development 
+            web development. These langugages include 
         </p>
       </div>
     </section>
@@ -16,3 +17,4 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
+
